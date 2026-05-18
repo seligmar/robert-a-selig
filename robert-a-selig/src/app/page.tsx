f6e36138eafe178cdcb33f06cb38cadcbadb57e9
github username <github email address>
2026-05-18 15:07:56 +0200

    build header

--- robert-a-selig/src/app/page.tsx
+++ robert-a-selig/src/app/page.tsx
@@ -2,14 +2,13 @@ import "./page.css"
 
 export default function Home() {
   return (
-    <div className="background-grey">
-      <main >
-        <div >
-          <h1>To get started, edit the page.tsx file.</h1>
-        </div>
-        <div >
-        </div>
-      </main>
-    </div>
+    <><header className="header-outside">
+      <div className="header-inside">Robert A. Selig, PhD.</div>
+      </header>
+      <main className="background-grey">
+      <h1>To get started, edit the page.tsx file.</h1>
+      <div>
+      </div>
+    </main></>
   );
 }
