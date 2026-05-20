33e8e45c009dc84dc21d14fb83d505f32e84a9bb
github username <github email address>
2026-05-20 14:25:38 +0200

    build header

--- robert-a-selig/src/app/page.tsx
+++ robert-a-selig/src/app/page.tsx
@@ -5,10 +5,15 @@ export default function Home() {
     <><header className="header-footer-outside">
       <div className="header-footer-inside">Robert A. Selig, PhD.</div>
       </header>
+      <hr className="orange-border"/>
       <main className="background-grey">
       <h1>To get started, edit the page.tsx file.</h1>
     </main>
-    <footer className="header-footer-outside">Footer</footer>
+       <hr className="orange-border"/>
+    <footer className="header-footer-outside">Footer
+      Copyright 2026. 
+Website by: Mary Selig
+    </footer>
     </>
   );
 }
