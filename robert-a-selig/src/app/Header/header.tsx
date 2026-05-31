e15ad6c3f1fe87e3df6f3acd8a02ff3a687182eb
github username <github email address>
2026-05-31 13:18:00 +0200

    upate linkks

--- robert-a-selig/src/app/Header/header.tsx
+++ robert-a-selig/src/app/Header/header.tsx
@@ -1,8 +1,14 @@
 import Image from 'next/image'
+import Link from 'next/link'
+import { useRouter } from 'next/navigation'
 //  import * as list from "./list.svg"
 import "./header-footer.css"
 
 export default function Header() {
+  // useRouter
+// const router = useRouter()
+//router.push('/dashboard', { scroll: false })
+
   return (   
 <header className="header-footer-outside">
       <div className="header-footer-inside header-inside"><h1>
@@ -13,8 +19,10 @@ export default function Header() {
               /> */}
                 <div className='menu-outside'>
                   {/* https://blog.logrocket.com/css-style-button-accessibility/#button-accessibility */}
-         <a className='menu-link'>About</a> <a className='menu-link'>Publications</a><a className='menu-link'>Multi-Media</a><a className='menu-link'>Links</a>
-         <a className='menu-link'>Contact</a>
+                  <nav>
+         <Link className='menu-link' href={"/"}>About</Link>   <Link className='menu-link' href={"/publications"}>Publications</Link><Link className='menu-link' href={"/multi-media"}>Multi-Media</Link><Link className='menu-link' href={"/links"}>Links</Link>
+            
+         <Link className='menu-link' href={"/contact"}>Contact</Link>      </nav>
          </div>
         </div>        
       </header>
