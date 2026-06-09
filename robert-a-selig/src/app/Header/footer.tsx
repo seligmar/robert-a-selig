4e565413b195a5511493f07e2f41d1b311d9d3ad
github username <github email address>
2026-06-09 12:21:54 +0200

    update home page links

--- robert-a-selig/src/app/Header/footer.tsx
+++ robert-a-selig/src/app/Header/footer.tsx
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <footer className='header-footer-outside'>
       <div className='header-footer-inside footer-inside'>
-        Copyright 2026.
+        Copyright 2026
         <div>Website by: Mary Selig</div>
       </div>
     </footer>
--- robert-a-selig/src/app/about/page.tsx
+++ robert-a-selig/src/app/about/page.tsx
@@ -15,33 +15,44 @@ export default function HomePage() {
       <div className='home-content'>
         Robert A. Selig is a historical consultant who received his Ph.D. in
         history from the Universität Würzburg in Germany in 1988. He published a
-        number of books on the American War of Independence such as Hussars in
-        Lebanon! A Connecticut Town and Lauzun’s Legion during the American
-        Revolution, 1780-1781 (Lebanon, 2004) and a translation of A Treatise on
-        Partisan Warfare by Johann von Ewald. Introduction and Annotation by
-        Robert A. Selig and David Curtis Skaggs (Westport, 1991).
+        number of books on the American War of Independence such as{' '}
+        <a href='https://www.abebooks.com/first-edition/Hussars-Lebanon-Connecticut-town-Lauzuns-legion/32276164237/bd'>
+          <i>Hussars in Lebanon! A Connecticut Town</i>
+        </a>{' '}
+        and <i>Lauzun’s Legion during the American Revolution, 1780-1781</i>{' '}
+        (Lebanon, 2004) and a translation of{' '}
+        <i>A Treatise on Partisan Warfare</i> by Johann von Ewald, Introduction
+        and Annotation by Robert A. Selig and David Curtis Skaggs (Westport,
+        1991).
       </div>
       <div className='home-content'>
         He is a specialist on the role of French forces under the comte de
         Rochambeau during the American Revolutionary War and serves as project
-        historian to the National Park Service for the Washington-Rochambeau
-        Revolutionary Route National Historic Trail Project. For this project he
-        researched and wrote surveys and resource inventories for the states of
-        New Hampshire, Rhode Island, Connecticut, New York, New Jersey,
-        Pennsylvania, Delaware, Maryland, the District of Columbia and Virginia
-        through which American and French forces marched in 1781 and 1782. These
-        reports are available on the internet at:{' '}
+        historian to the National Park Service for the{' '}
+        <a href='https://www.nps.gov/waro/index.htm'>
+          Washington-Rochambeau Revolutionary Route National Historic Trail
+          Project
+        </a>
+        . For this project he researched and wrote surveys and resource
+        inventories for the states of New Hampshire, Rhode Island, Connecticut,
+        New York, New Jersey, Pennsylvania, Delaware, Maryland, the District of
+        Columbia and Virginia through which American and French forces marched
+        in 1781 and 1782. These reports are available on the internet at:{' '}
         <a>https://w3r-us.org/history-by-state/.</a>
       </div>
       <div className='home-content'>
-        He also served and serves as Project Historian for American Battlefield
-        Protection Program projects such as the “Battle of Princeton”, “Battle
-        of Green Spring and Spencer’s Ordinary”, the “Paoli Massacre”, “Battle
-        of the Clouds”, “Battle of Red Bank”, “Battle of Bennington”, “Battle of
-        Hubbardton” and the “Battle of Brandywine”.
+        He also served and serves as Project Historian for{' '}
+        <a href='https://www.nps.gov/orgs/2287/index.htm'>
+          American Battlefield Protection Program
+        </a>{' '}
+        projects such as the “Battle of Princeton”, “Battle of Green Spring and
+        Spencer’s Ordinary”, the “Paoli Massacre”, “Battle of the Clouds”,
+        “Battle of Red Bank”, “Battle of Bennington”, “Battle of Hubbardton” and
+        the “Battle of Brandywine”.
       </div>
       <div className='home-content'>
-        He has been a regular contributor to German Life magazine for over 20
+        He has been a regular contributor to{' '}
+        <a href='https://germanlife.com/'>German Life</a> magazine for over 20
         years and has also published more than 100 articles in American and
         German scholarly and popular history magazines such as the the William
         and Mary Quarterly, Eighteenth-Century Studies, the Yearbook of the
@@ -52,7 +63,7 @@ export default function HomePage() {
         books and anthologies.
       </div>
       <div className='home-content'>
-        Dr. Robert A. Selig's CV is available<a>here</a>.
+        Dr. Robert A. Selig's CV is available <a>here</a>.
       </div>
     </>
   );
