6788c6478ed83ee636150c6a8c93fceeea1bdfac
github username <github email address>
2026-06-11 17:49:37 +0200

    cv page not yet working

--- robert-a-selig/src/app/cv/page.tsx
+++ robert-a-selig/src/app/cv/page.tsx
@@ -1,11 +1,29 @@
-import Image from 'next/image';
 import PageLayout from '../layout/pagelayout';
+//import cvToShow from './SeligCV.pdf';
 
 export default function cv() {
   return (
     <>
       <PageLayout>
         <h2>Curriculum Vitae</h2>
+        <iframe src={'SeligCV.pdf'} width='600' height='500' />
+        <object
+          data='SeligCV.pdf'
+          type='application/pdf'
+          width='100%'
+          height='100%'
+        >
+          <p>
+            Unable to display PDF file. <a href='sample.pdf'>Download</a>{' '}
+            instead.
+          </p>
+        </object>
+        <embed
+          src={'SeligCV.pdf'}
+          type='application/pdf'
+          width='600'
+          height='500'
+        />
       </PageLayout>
     </>
   );
