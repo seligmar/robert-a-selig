d0d51e5681afaa95272ebd6f9bf4999e736a320f
github username <github email address>
2026-06-11 17:58:05 +0200

    cv page add css

--- robert-a-selig/src/app/cv/page.tsx
+++ robert-a-selig/src/app/cv/page.tsx
@@ -6,24 +6,9 @@ export default function cv() {
     <>
       <PageLayout>
         <h2>Curriculum Vitae</h2>
-        <iframe src={'SeligCV.pdf'} width='600' height='500' />
-        <object
-          data='SeligCV.pdf'
-          type='application/pdf'
-          width='100%'
-          height='100%'
-        >
-          <p>
-            Unable to display PDF file. <a href='sample.pdf'>Download</a>{' '}
-            instead.
-          </p>
-        </object>
-        <embed
-          src={'SeligCV.pdf'}
-          type='application/pdf'
-          width='600'
-          height='500'
-        />
+        <div className='centre-cv'>
+          <iframe src={'SeligCV.pdf'} width='600' height='500' />
+        </div>
       </PageLayout>
     </>
   );
