e9f2891d5b0ff383e846e2b1762816a52cf7377e
github username <github email address>
2026-06-15 12:05:23 +0200

    add multimedia page

--- robert-a-selig/src/app/multimedia/page.tsx
+++ robert-a-selig/src/app/multimedia/page.tsx
@@ -4,25 +4,44 @@ export default function multimedia() {
   return (
     <>
       <PageLayout>
-        <h2>Multi-Media Video Presentations</h2>
-        VIDEO PRESENTATIONS: Waging War in America, 1775-1783: Operational
-        Challenges of Five Armies. Panel discussion hosted by the American
-        Revolution Institute of The Society of the Cincinnati on 1 August 2024:
-        https://www.youtube.com/watch?v=yoAHlGDjINI Rochambeau at Morven:
-        https://youtu.be/nkx64Q5ACgc The March to Yorktown:
-        https://www.c-span.org/video/?327130-1/discussion-march-yorktown
-        Revolutionary War Military Burials:
-        https://www.c-span.org/video/?453830-2/revolutionary-war-military-burials
-        Tuesday, 14 August 1781. The Day That Shook The World:
-        https://youtu.be/r0qGvd2P7qI Five podcasts in the series Amazing Tales
-        from Off and On Connecticut‘s Beaten Path broadcast by Mike Allen:
-        https://amazingtalesct.podbean.com/e/could-you-move-a-5000-man-foreign-speaking-army-across-colonial-ct/
-        Wednesday, 16 August 2023: George Washington Slept Here - Or Did He?
-        Wednesday, 22 March 2023: When Hungarian Soldiers Came to Rural
-        Connecticut for the Winter Wednesday, 2 February 2022: A Trail Like No
-        Other - It Brought Us Freedom (Part 2) Wednesday, 26 January 2022: A
-        Trail Like No Other - It Brought Us Freedom (Part 1; about the
-        Washington-Rochambeau Revolutionary Route National Historic Trail)
+        <h2>Multi-Media Presentations</h2>
+        <iframe
+          width='420'
+          height='315'
+          src='https://www.youtube.com/watch?v=yoAHlGDjINI'
+        ></iframe>
+        Waging War in America, 1775-1783: Operational Challenges of Five Armies.
+        Panel discussion hosted by the American Revolution Institute of The
+        Society of the Cincinnati on 1 August 2024:
+        https://www.youtube.com/watch?v=yoAHlGDjINI
+        <div>Rochambeau at Morven: https://youtu.be/nkx64Q5ACgc</div>{' '}
+        <div>
+          The March to Yorktown:
+          https://www.c-span.org/video/?327130-1/discussion-march-yorktown
+        </div>
+        <div>
+          {' '}
+          Revolutionary War Military Burials:
+          https://www.c-span.org/video/?453830-2/revolutionary-war-military-burials
+        </div>
+        <div>
+          Tuesday, 14 August 1781. The Day That Shook The World:
+          https://youtu.be/r0qGvd2P7qI{' '}
+        </div>
+        <div>
+          Five podcasts in the series Amazing Tales from Off and On
+          Connecticut‘s Beaten Path broadcast by Mike Allen:
+          https://amazingtalesct.podbean.com/e/could-you-move-a-5000-man-foreign-speaking-army-across-colonial-ct/
+        </div>
+        <div>
+          {' '}
+          Wednesday, 16 August 2023: George Washington Slept Here - Or Did He?
+          Wednesday, 22 March 2023: When Hungarian Soldiers Came to Rural
+          Connecticut for the Winter Wednesday, 2 February 2022: A Trail Like No
+          Other - It Brought Us Freedom (Part 2) Wednesday, 26 January 2022: A
+          Trail Like No Other - It Brought Us Freedom (Part 1; about the
+          Washington-Rochambeau Revolutionary Route National Historic Trail)
+        </div>
       </PageLayout>
     </>
   );
