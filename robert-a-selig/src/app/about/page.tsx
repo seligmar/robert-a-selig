0ec6ad8cb76c0ce1cf7709a5fc297a92bd30e2ad
github username <github email address>
2026-06-24 13:02:25 +0200

    updated links

--- robert-a-selig/src/app/about/page.tsx
+++ robert-a-selig/src/app/about/page.tsx
@@ -17,8 +17,20 @@ export default function HomePage() {
       />
       <div className='home-content'>
         Robert A. Selig is a historical consultant who received his Ph.D. in
-        history from the Universität Würzburg in Germany in 1988. He published a
-        number of books on the American War of Independence such as{' '}
+        history from the <i>Universität Würzburg</i> in Germany in 1988. He is a
+        specialist on the role of French forces under the <i>comte</i> de
+        Rochambeau during the American War of Independence and serves as project
+        historian for the{' '}
+        <a href='https://www.nps.gov/waro/index.htm' target='_blank'>
+          Washington-Rochambeau Revolutionary Route National Historic Trail
+          Project
+        </a>
+        . For this project he researched and wrote surveys and resource
+        inventories for the nine states through which American and French forces
+        marched in 1781 and 1782.
+      </div>
+      <div className='home-content'>
+        Among his publications are{' '}
         <a
           href='https://www.abebooks.com/first-edition/Hussars-Lebanon-Connecticut-town-Lauzuns-legion/32276164237/bd'
           target='_blank'
@@ -26,24 +38,38 @@ export default function HomePage() {
           <i>Hussars in Lebanon! A Connecticut Town</i>
         </a>{' '}
         and <i>Lauzun’s Legion during the American Revolution, 1780-1781</i>{' '}
-        (Lebanon, 2004) and a translation of{' '}
-        <i>A Treatise on Partisan Warfare</i> by Johann von Ewald, Introduction
-        and Annotation by Robert A. Selig and David Curtis Skaggs (Westport,
-        1991).
+        (Lebanon, 2004), and some 150 articles in American, German, and French
+        scholarly and popular history magazines such as the{' '}
+        <i>William and Mary Quarterly</i>, <i>Eighteenth-Century Studies</i>,
+        the <i>Yearbook of the Society for German-American Studies</i>, as well
+        as chapters in books and anthologies. His most recent publications
+        include “Damages, Depredations, Sufferings and Destruction: The
+        Landscape of Conflict and the ‘Late War with Great Britain’”. In: The
+        Archaeology of the American Revolution Richard F. Veit and Matthew A.
+        Kalos, eds., (Gainesville: University Press of Florida, 2025 pp.
+        205-227. (with Wade P. Catts); “The Culture of Death: An Overview of
+        Burial Practices during the American War of Independence.” The Brigade
+        Dispatch. Journal Of The Brigade Of The American Revolution vol. 50 No.
+        1, (Winter 2023-2024), pp. 3-18; “L’expédition particulière and the
+        American War of Independence, 1780–1783." in: Waging War in
+        America 1775-1783. Operational Challenges of Five Armies Don N. Hagist,
+        ed. (Warwick: Helion & Company, 2023), pp. 171-191; « De Newport à
+        Yorktown, à la Victoire et à la Gloire: la Route Washington-Rochambeau
+        et la Victoire du 19 octobre » 1781 Bulletin Société de la Archéologique
+        Scientifique et Littéraire du Vendômois Année 2022, pp. 99-104.
       </div>
+      (Lebanon, 2004) and a translation of <i>A Treatise on Partisan Warfare</i>{' '}
+      by Johann von Ewald, Introduction and Annotation by Robert A. Selig and
+      David Curtis Skaggs (Westport, 1991).
       <div className='home-content'>
         He is a specialist on the role of French forces under the comte de
         Rochambeau during the American Revolutionary War and serves as project
-        historian to the National Park Service for the{' '}
-        <a href='https://www.nps.gov/waro/index.htm' target='_blank'>
-          Washington-Rochambeau Revolutionary Route National Historic Trail
-          Project
-        </a>
-        . For this project he researched and wrote surveys and resource
-        inventories for the states of New Hampshire, Rhode Island, Connecticut,
-        New York, New Jersey, Pennsylvania, Delaware, Maryland, the District of
-        Columbia and Virginia through which American and French forces marched
-        in 1781 and 1782. These reports are available on the internet at:{' '}
+        historian to the National Park Service for the . For this project he
+        researched and wrote surveys and resource inventories for the states of
+        New Hampshire, Rhode Island, Connecticut, New York, New Jersey,
+        Pennsylvania, Delaware, Maryland, the District of Columbia and Virginia
+        through which American and French forces marched in 1781 and 1782. These
+        reports are available on the internet at:{' '}
         <a>https://w3r-us.org/history-by-state/.</a>
       </div>
       <div className='home-content'>
