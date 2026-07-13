47ad77d9908a5b7144e1493381f5f7639580d960
github username <github email address>
2026-07-13 16:34:08 +0200

    move impages to public file

--- robert-a-selig/src/app/Header/footer.tsx
+++ robert-a-selig/src/app/Header/footer.tsx
@@ -1,11 +1,18 @@
+import Image from 'next/image';
 import './header-footer.css';
+import { Camera } from 'lucide-react';
 
 export default function Footer() {
   return (
     <footer className='header-footer-outside'>
       <div className='header-footer-inside footer-inside'>
-        
-        <div> Copyright 2026</div>
+        <link
+          rel='icon'
+          href='/W3rShieldLogo.svg'
+          type='image/W3rShieldLogo.svg'
+          // sizes='<generated>'
+        />
+        <Camera />;<div> Copyright 2026</div>
         <div>Website by: Mary Selig</div>
       </div>
     </footer>
--- robert-a-selig/src/app/layout.tsx
+++ robert-a-selig/src/app/layout.tsx
@@ -7,6 +7,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='en'>
+      <link
+        rel='icon'
+        href='/W3rShieldLogo.ico'
+        type='image/W3rShieldLogo.ico'
+      //  sizes='<generated>'
+      />
       <title>Dr. Robert A. Selig, PhD.</title>
       <body>{children}</body>
     </html>
