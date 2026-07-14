7bb8236f4f3521733de29a9e871032fdae7ed5ec
github username <github email address>
2026-07-14 16:12:36 +0200

    update links

--- robert-a-selig/src/app/Header/footer.tsx
+++ robert-a-selig/src/app/Header/footer.tsx
@@ -1,12 +1,35 @@
 import Image from 'next/image';
 import './header-footer.css';
-import { Camera } from 'lucide-react';
 
 export default function Footer() {
   return (
     <footer className='header-footer-outside'>
       <div className='header-footer-inside footer-inside'>
-        <Camera />
+        <div className='icon-div'>
+          <a
+            href='https://www.linkedin.com/in/robert-a-selig-492b7612/'
+            target='_blank'
+            className='linkedin-link'
+          >
+            {' '}
+            <Image
+              src='/linkedinLogo.svg'
+              alt='linkedIn'
+              width={40}
+              height={40}
+              className='footer-link'
+            />
+          </a>
+          <a href='https://www.facebook.com/robert.a.selig.7'>
+            <Image
+              src='/facebookIcon.png'
+              alt='facebook'
+              width={40}
+              height={40}
+              // className='footer-link'
+            />
+          </a>
+        </div>
         <div> Copyright 2026 Website by: Mary Selig</div>
       </div>
     </footer>
