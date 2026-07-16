f7cfd5f3c420548473f5312679af46365fc26649
github username <github email address>
2026-07-16 17:01:02 +0200

    move impages to public file

--- robert-a-selig/src/app/layout.tsx
+++ robert-a-selig/src/app/layout.tsx
@@ -7,12 +7,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='en'>
-   <link
-  rel="icon"
-  href="/icon?<generated>"
-  type="image/<generated>"
-  sizes="<generated>"
-/>
+      <link
+        rel='icon'
+        href='/icon?<generated>'
+        type='image/<generated>'
+        sizes='<generated>'
+      />
       <title>Dr. Robert A. Selig, PhD.</title>
       <body>{children}</body>
     </html>
