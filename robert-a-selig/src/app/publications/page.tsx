d0724ecebad00312cdd0b5903730674cd23e9bce
github username <github email address>
2026-07-22 17:15:11 +0200

    clean up publications

--- robert-a-selig/src/app/publications/page.tsx
+++ robert-a-selig/src/app/publications/page.tsx
@@ -48,8 +48,8 @@ export default function publications() {
               “Damages, Depredations, Sufferings and Destruction: The Landscape
               of Conflict and the ‘Late War with Great Britain’”. In:{' '}
               <i>The Archaeology of the American Revolution</i> Richard F. Veit
-              and Matthew A. Kalos, eds., Gainesville: University Press of
-              Florida, 2025 pp. 205-227 (With Wade P. Catts).
+              and Matthew A. Kalos, eds., (Gainesville: University Press of
+              Florida, 2025) pp. 205-227 (With Wade P. Catts).
             </div>
             <div className='publication-margin'>
               Rochambeau's Engineers at Butts Hill Fort, July 1780 - June 1781.
